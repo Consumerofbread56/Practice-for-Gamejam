16183e658ca6da35543d447910c6685f3c69931a
Consumerofbread56 <96402371+Consumerofbread56@users.noreply.github.com>
2026-01-26 16:50:06 -0500

    Update sketch.js

--- practice for da game jam/sketch.js
+++ practice for da game jam/sketch.js
@@ -1,10 +1,25 @@
 
+s = false
+p = true
+
+class Player {
+
+}
+
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(220);
+}
+
+function loadRoom() {
+  if (s === false || p === false) {
+    print("nigga")
+  }
 }
