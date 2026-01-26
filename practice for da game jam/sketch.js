d0538bff2082f59d222c63d2157a77775529617a
Consumerofbread56 <96402371+Consumerofbread56@users.noreply.github.com>
2026-01-26 18:35:29 -0500

    to publish

--- practice for da game jam/sketch.js
+++ practice for da game jam/sketch.js
@@ -3,7 +3,20 @@
 class Player {
 
 }
-rooms = fetch(roomData.txt)
+
+
+
+const ROOMDATA = fetch('https://github.com/Consumerofbread56/Practice-for-Gamejam/blob/b14ad7c8ea6cba92fe3527f17b046e07379807e5/practice%20for%20da%20game%20jam/roomData.txt')
+
+.then((response) => {
+  if (!response.ok) {
+    console.log("There was an error with fetching room data.");
+  }
+  console.log(response);
+   return response.text();
+   
+  })
+ 
 
 
 
