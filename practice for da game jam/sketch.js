9965a28cb96a35af85601d76d8ec4ceba2ac3c9d
Consumerofbread56 <96402371+Consumerofbread56@users.noreply.github.com>
2026-01-26 20:32:29 -0500

    More FUCKING testing

--- practice for da game jam/sketch.js
+++ practice for da game jam/sketch.js
@@ -4,28 +4,24 @@ class Player {
 
 }
 
-roomVals = getRoomData();
+let roomData;
 
-async function getRoomData() {
-  try {
-    const response = fetch('https://raw.githubusercontent.com/Consumerofbread56/Practice-for-Gamejam/refs/heads/main/practice%20for%20da%20game%20jam/roomData.txt')
+fetch('https://raw.githubusercontent.com/Consumerofbread56/Practice-for-Gamejam/refs/heads/main/practice%20for%20da%20game%20jam/roomData.txt')
+    .then(response => {
     if (!response.ok) {
       console.log("There was an error with fetching room data.");
     }
-  
-    console.log(response.text());
-    const ROOMDATA = await response.text();
-    return ROOMDATA;
-
-  } catch (error) {
-    console.log("There was a separate error or sm idk");
-  }
     
-}
+    return response.text();
 
+    })
+    .then(data => {
+      roomData = data;
+    })
 
-
-
+   .catch (error => {
+    console.log("There was a separate error or sm idk");
+  })
 
 
 
@@ -35,7 +31,7 @@ async function getRoomData() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  console.log(roomVals);
+  console.log(roomData);
 }
 
 function draw() {
